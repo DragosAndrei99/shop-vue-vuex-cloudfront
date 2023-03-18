542755d6b798cfcc3f778138d831b1c4cce7058e
Dragos Vasile <vdragos1999@yahoo.com>
2023-03-18 10:08:31 +0200

    Task-7

--- src/constants/api-paths.ts
+++ src/constants/api-paths.ts
@@ -1,7 +1,7 @@
 export const API_PATHS = {
-	product: 'https://fqqlix35v5.execute-api.us-east-1.amazonaws.com/dev',
+	product: 'https://g4io1zjjg9.execute-api.us-east-1.amazonaws.com/dev',
 	order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-	import: 'https://umo0lhn8cb.execute-api.us-east-1.amazonaws.com/dev',
+	import: 'https://7yacl0vv67.execute-api.us-east-1.amazonaws.com/dev',
 	bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
